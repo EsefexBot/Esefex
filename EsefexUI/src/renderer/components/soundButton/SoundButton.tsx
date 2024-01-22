4e8cc1df6e754a4814f32d22544c2b10b58a3ac5
Patrick Cerny <patrick.cerny04@gmail.com>
2024-01-22 23:29:17 +0100

    Fixed es-lint error in button component

--- EsefexUI/src/renderer/components/soundButton/SoundButton.tsx
+++ EsefexUI/src/renderer/components/soundButton/SoundButton.tsx
@@ -1,15 +1,18 @@
-import { Button } from "@mantine/core";
-import { FC } from "react";
-import { Sound } from "../../models/Sound";
+import { Button } from '@mantine/core';
+import { Sound } from '../../models/Sound';
 
 interface SoundButtonProps {
-    sound: Sound;
+  sound: Sound;
 }
 
-export const SoundButton: FC<SoundButtonProps> = ({sound}) => {
-    return (
-        <>
-            <Button w={100} h={100} bg="#1A1A1A" radius={25}><img src={sound.icon}/></Button>
-        </>
-    );
+function SoundButton(props: SoundButtonProps) {
+  const { sound } = props;
+
+  return (
+    <Button w={100} h={100}>
+      <img src={sound.icon} alt="test" />
+    </Button>
+  );
 }
+
+export default SoundButton;
